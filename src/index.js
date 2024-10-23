39e144dc785a46757077929085fa65cb9cb1e876
Allen L Koickal <73332153+Allenlexy@users.noreply.github.com>
2024-10-24 00:31:39 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom"; // Note: 'react-dom/client' is not available in React 17
 
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+// Use ReactDOM.render for React 17
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
